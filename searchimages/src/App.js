f2e194576edef1840803db05e85ed65feeb6df23
Victor Torres Mori <vatorresm88@gmail>
2019-06-13 13:35:59 -0500

    Consuminedo el api de Pixabay

--- searchimages/src/App.js
+++ searchimages/src/App.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Buscador from './Componentes/Buscador';
+import Resultado from './Componentes/Resultado';
 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="app container">
-      <div className="jumbotron">
-        <p className="lead text-center">Buscador de Imágenes</p>
+  state = {
+    termino : '',
+    imagenes : []
+  }
+
+  
+
+  consultarApi = () => {
+    const termino = this.state.termino;
+    const url = `https://pixabay.com/api/?key=12767615-0aa6dc1c2b8a7f9407f9d6a0f&q=${termino}&per_page=30`;
+  
+    //console.log(url);
+    fetch(url)
+      .then(respuesta => respuesta.json())
+      .then(resultado => this.setState({ imagenes: resultado.hits }))
+
+  }
+
+  datosBusqueda = (termino) => {
+    this.setState({
+      termino
+    }, () => {
+      this.consultarApi();
+    })
+  }
+
+  render() {
+    return (
+      <div className="app container">
+        <div className="jumbotron">
+          <p className="lead text-center">Buscador de Imágenes</p>
+          <Buscador
+            datosBusqueda={this.datosBusqueda}
+          />
+        </div>
+        <div className="row justify-content-center">
+          <Resultado
+            imagenes={this.state.imagenes}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
---
+++ searchimages/src/Componentes/Buscador.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Buscador extends Component {
+
+    busquedaRef = React.createRef();
+
+    obtenerDatos = (e) => {
+        e.preventDefault();
+
+        const termino = this.busquedaRef.current.value;
+
+        this.props.datosBusqueda(termino);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.obtenerDatos}>
+                <div className="row">
+                 
+                    <div className="form-group col-md-8">
+                        <input ref={this.busquedaRef} type="text" className="form-control 
+                        form-control-lg" placeholder="Busca tu imagen. 
+                        Ejemplo: Soccer"/>
+                    </div>
+
+                    <div className="form-group col-md-4">
+                        <input type="submit" className="btn btn-warning btn-lg btn-block" 
+                        value="Buscar..." />
+                    </div>
+                </div>
+            </form>
+         );
+    }
+}
+
+export default Buscador;
